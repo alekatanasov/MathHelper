1b5d03c6698ec9363a0a60dfba8a42dbbed7f851
alekatanasov <alekyatanasov@gmail.com>
2019-10-21 23:45:11 +0300

    new tests

--- test/parse/MathStatementParserTest.java
+++ test/parse/MathStatementParserTest.java
@@ -1,34 +1,67 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
 package parse;
 
+
 import interfaces.expression.MathSymbol;
-import org.junit.After;
-import org.junit.AfterClass;
+import java.util.Arrays;
+import java.util.Collection;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
 /**
  *
  * @author Alexander Atanasov
  */
+@RunWith(Parameterized.class)
 public class MathStatementParserTest {
+    private MathStatementParser parser;
+    private String input;
+    private String expectedOutput;
     
-    public MathStatementParserTest() {
+    public MathStatementParserTest(String input, String output) {
+        this.input = input;
+        this.expectedOutput = output;
     }
 
+    @Before
+    public void initialize(){
+        this.parser = new MathStatementParser();
+    }
+    
+    @Parameterized.Parameters
+    public static Collection mathStatements() {
+        return Arrays.asList(new Object[][] {
+            { "1+3", "1+3" },
+            { "1-3", "1-3" },
+            { "1*3", "1*3" },
+            { "2^3", "2^3" },
+            { "-1+3", "-1+3" },
+            { "(1+2-3)", "(1+2-3)" },
+            { "(-2^2/3)", "(-2^2/3)" }
+            
+      });
+   }
+    
     /**
      * 
      */
     @Test
     public void testMathStatementParser() {
+        String parsedStatement = "";
+        MathSymbol[] mathStatement;
         
+        this.parser.parseStatement(this.input);
+        mathStatement = this.parser.popLastParsedStatement();
+        for(int c=0; c<mathStatement.length; c++){
+            parsedStatement+= mathStatement[c].getMathSymbol();
+        }
+        
+        System.out.println("Math statement is: " + this.input);
+        assertEquals(this.expectedOutput,parsedStatement);
     }
     
 }
